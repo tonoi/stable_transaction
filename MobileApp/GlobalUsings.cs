8aef40e36b28c26eddd05b4bd045b937500bfd9e
Mitsuo-Koikawa <mkoikawa@tonoi.co.jp>
2025-06-09 14:20:33 +0900

    Mobile App Build error 解消

--- MobileApp/GlobalUsings.cs
+++ MobileApp/GlobalUsings.cs
@@ -1,3 +1,7 @@
 global using JPYCOffline.Services;
 global using JPYCOffline.ViewModels;
 global using JPYCOffline.Pages;
+global using Microsoft.Maui.Controls;
+global using Microsoft.Maui.Controls.Xaml;
+global using ZXing.Net.Maui;
+global using ZXing.Net.Maui.Controls;
